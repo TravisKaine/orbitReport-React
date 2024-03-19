2cc531de6c239ed247b40bb0933c311f53d5c3fb
Your Name <traviskaine>
2024-03-18 20:00:43 -0500

    did it and a little extra

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import React, { useState } from 'react';
+import satData from './components/satData';
+import Banner from './components/Banner';
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+
+  const displaySats = [...new Set(satData.map(data => data.orbitType))];
+
+  const filterByType = currentType => {
+    const filteredSat = satData.filter(newSatDisplay => newSatDisplay.orbitType === currentType);
+    setSat(filteredSat);
+  };
+
   return (
-    <div>
-      <Buttons />
-      <Table />
-    </div>
+    <>
+      <div className="flier"><img src="https://cdn.pixabay.com/photo/2017/09/21/07/57/satellite-2771043_640.png" alt="Image 1" /></div>
+      <Banner />
+      <Buttons
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
+      />
+      <Table sat={sat} />
+    </>
   );
 }
 
